bb6869128a388d2147cdd5986a505fcf862e49b8
Petri Autio <petri.mikael.autio@gmail.com>
2016-08-10 23:14:01 +0100

    Added dashboard lamps for planetary targeting, colour coordination and improved texts.

--- 18 Quintillion/Assets/Scripts/GameController.cs
+++ 18 Quintillion/Assets/Scripts/GameController.cs
@@ -7,6 +7,10 @@ public class GameController : MonoBehaviour {
 
     public GameObject worldNumberText;
     public GameObject mainText;
+    private float lampCounter = 2.0f / 9;
+    public GameObject[] lamps;
+    public Sprite[] lampSprites;
+    int currentLamp = 0;
     List<string> part1 = new List<string>();
     List<string> part2 = new List<string>();
     List<string> part3 = new List<string>();
@@ -41,9 +45,9 @@ public class GameController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         part1.Add("mainly ");
-        part1.Add("slightly ");
+        part1.Add("fabulously ");
         part1.Add("wholly ");
-        part1.Add("hardly ");
+        part1.Add("remarkably ");
         part1.Add("glaringly ");
         part1.Add("subtly ");
         part1.Add("entirely ");
@@ -52,10 +56,10 @@ public class GameController : MonoBehaviour {
 
         part2.Add("pink, ");
         part2.Add("green, ");
-        part2.Add("gray, ");
+        part2.Add("grey, ");
         part2.Add("brown, ");
         part2.Add("blue, ");
-        part2.Add("black, ");
+        part2.Add("bland, ");
         part2.Add("orange, ");
         part2.Add("yellow, ");
         part2.Add("red, ");
@@ -68,9 +72,9 @@ public class GameController : MonoBehaviour {
         part3.Add("geophysically ");
         part3.Add("atmospherically ");
         part3.Add("religiously ");
-        part3.Add("speculatively ");
+        part3.Add("ideologically ");
         part3.Add("heart-breakingly ");
-        part3.Add("psychedelically ");
+        part3.Add("philosophically ");
 
         part4.Add("exceptional, ");
         part4.Add("unremarkable, ");
@@ -79,7 +83,7 @@ public class GameController : MonoBehaviour {
         part4.Add("amusing, ");
         part4.Add("flourishing, ");
         part4.Add("wearisome, ");
-        part4.Add("trembling, ");
+        part4.Add("suspect, ");
         part4.Add("legendary, ");
         part4.Add("porous, ");
 
@@ -87,10 +91,10 @@ public class GameController : MonoBehaviour {
         part5.Add("dense and ");
         part5.Add("dented and ");
         part5.Add("cracked and ");
-        part5.Add("shattered but ");
-        part5.Add("");
+        part5.Add("catastrophically shattered, ");
+        part5.Add("delightfully ");
         part5.Add("invitingly ");
-        part5.Add("smooth, ");
+        part5.Add("incredibly smooth, ");
         part5.Add("");
         part5.Add("");
 
@@ -118,25 +122,25 @@ public class GameController : MonoBehaviour {
 
         part8.Add("heavy with ");
         part8.Add("richly endowed with ");
-        part8.Add("with just about enough of ");
+        part8.Add("with just about enough harvestable ");
         part8.Add("with heaps and heaps of ");
         part8.Add("replete with millions of tons of ");
         part8.Add("with modest reserves of ");
         part8.Add("bountifully laden with ");
         part8.Add("remarkable for its rich deposits of ");
         part8.Add("stocked with low-quality ");
-        part8.Add(", a veritable stockpile of ");
+        part8.Add("about to make us so very happy once we lay hands on its untouched reserves of ");
 
-        part9.Add("firewood, ");
-        part9.Add("coal, ");
-        part9.Add("oil, ");
-        part9.Add("uranium, ");
-        part9.Add("antimatter, ");
-        part9.Add("etherium, ");
-        part9.Add("ununpentium, ");
-        part9.Add("dilithium, ");
-        part9.Add("melange, ");
-        part9.Add("comprehension of the Absolute, ");
+        part9.Add("FIREWOOD, ");
+        part9.Add("COAL, ");
+        part9.Add("OIL, ");
+        part9.Add("URANIUM, ");
+        part9.Add("ANTIMATTER, ");
+        part9.Add("ELERIUM, ");
+        part9.Add("UNUNTRIUM, ");
+        part9.Add("DILITHIUM, ");
+        part9.Add("MELANGE, ");
+        part9.Add("COMPREHENSION OF THE ABSOLUTE, ");
 
         part10.Add("its meadows ");
         part10.Add("its caverns ");
@@ -147,12 +151,12 @@ public class GameController : MonoBehaviour {
         part10.Add("its arctic zones ");
         part10.Add("its surface ");
         part10.Add("its innards ");
-        part10.Add("its peaks and hills ");
+        part10.Add("its deserts ");
 
         part11.Add("full of ");
         part11.Add("dotted with ");
         part11.Add("harbouring ");
-        part11.Add("devoid of ");
+        part11.Add("remarkable for their ");
         part11.Add("dominated by ");
         part11.Add("partially covered by ");
         part11.Add("sadly lacking any ");
@@ -183,7 +187,7 @@ public class GameController : MonoBehaviour {
         part13.Add("yams ");
 
         part14.Add("and ");
-        part14.Add("but not ");
+        part14.Add("found within ");
         part14.Add("containing ");
         part14.Add("overshadowed by ");
         part14.Add("leeching off ");
@@ -202,29 +206,29 @@ public class GameController : MonoBehaviour {
         part15.Add("poetry-inducing ");
         part15.Add("procedurally generated ");
         part15.Add("velvety ");
-        part15.Add("pulsating ");
+        part15.Add("severely endangered ");
 
-        part16.Add("shrubs, ");
-        part16.Add("roots, ");
-        part16.Add("spruces, ");
-        part16.Add("vines, ");
-        part16.Add("pickles, ");
-        part16.Add("cactuses, ");
-        part16.Add("lichen, ");
-        part16.Add("seaweed, ");
-        part16.Add("pistachioes, ");
-        part16.Add("baobabs, ");
+        part16.Add("shrubs ");
+        part16.Add("roots ");
+        part16.Add("spruces ");
+        part16.Add("vines ");
+        part16.Add("pickles ");
+        part16.Add("cacti ");
+        part16.Add("lichen ");
+        part16.Add("seaweed ");
+        part16.Add("pistachioes ");
+        part16.Add("baobabs ");
 
-        part17.Add("inhabited by ");
-        part17.Add("sheltering ");
-        part17.Add("eaten by ");
-        part17.Add("guarded by ");
-        part17.Add("guarding ");
-        part17.Add("entirely ignored by ");
-        part17.Add("prized by ");
-        part17.Add("tended by ");
-        part17.Add("giving sustenance to ");
-        part17.Add("excreted on by ");
+        part17.Add("which feed the memorably ");
+        part17.Add("which provide shelter for the ");
+        part17.Add("frequently trapping the ");
+        part17.Add("jealously guarded by ");
+        part17.Add("somehow guarding ");
+        part17.Add("entirely ignored by the ");
+        part17.Add("prized by the ");
+        part17.Add("tended by the ");
+        part17.Add("giving sustenance to the ");
+        part17.Add("excreted on by the ");
 
         part18.Add("finned, ");
         part18.Add("birdlike, ");
@@ -255,31 +259,31 @@ public class GameController : MonoBehaviour {
         part20.Add("rock-");
         part20.Add("cat-");
         part20.Add("lava-");
-        part20.Add("");
+        part20.Add("mind-");
         part20.Add("ant-");
         part20.Add("tango-");
 
-        part21.Add("things, ");
-        part21.Add("crabs, ");
-        part21.Add("baboons, ");
-        part21.Add("moths, ");
-        part21.Add("lemurs, ");
-        part21.Add("chameleons, ");
-        part21.Add("wallabies, ");
-        part21.Add("devils, ");
-        part21.Add("warblers, ");
-        part21.Add("pidgeons, ");
+        part21.Add("things ");
+        part21.Add("crabs ");
+        part21.Add("baboons ");
+        part21.Add("moths ");
+        part21.Add("lemurs ");
+        part21.Add("chameleons ");
+        part21.Add("wallabies ");
+        part21.Add("devils ");
+        part21.Add("warblers ");
+        part21.Add("pidgeons ");
 
-        part22.Add("not forgetting the prevalent ");
-        part22.Add("in a wary truce with the apex predators: ");
+        part22.Add("not forgetting the ");
+        part22.Add("who exist in a wary truce with the apex predators, the  ");
         part22.Add("to be much preferred over the ");
-        part22.Add("cohabiting the southern part with ");
-        part22.Add("cohabiting the northern part with ");
-        part22.Add("always gleefully pestering the ");
-        part22.Add("preying upon the ");
+        part22.Add("cohabiting the southern part with groups of ");
+        part22.Add("cohabiting the northern part with groups of ");
+        part22.Add("who can be found always gleefully pestering the ");
+        part22.Add("- nasty things who prey upon the ");
         part22.Add("lazing amidst hordes and hordes of ");
         part22.Add("singing in polyphony with a small grouping of ");
-        part22.Add("and ");
+        part22.Add("and the ");
 
         part23.Add("musical ");
         part23.Add("wobbling ");
@@ -288,7 +292,7 @@ public class GameController : MonoBehaviour {
         part23.Add("prickly ");
         part23.Add("ticklish ");
         part23.Add("annoying ");
-        part23.Add("toroidal ");
+        part23.Add("topologically challenged ");
         part23.Add("two-faced ");
         part23.Add("bellicose ");
 
@@ -303,7 +307,7 @@ public class GameController : MonoBehaviour {
         part24.Add("sticks ");
         part24.Add("trombones ");
 
-        part25.Add("and nations of ");
+        part25.Add("which lead us to a nation of ");
         part25.Add("and tribes of ");
         part25.Add("reared by communities of ");
         part25.Add("hunting families of ");
@@ -312,7 +316,7 @@ public class GameController : MonoBehaviour {
         part25.Add("projected to evolve into ");
         part25.Add("pleading to become ");
         part25.Add("domesticated by a civilisation of ");
-        part25.Add("carefully cultivated by ");
+        part25.Add("carefully cultivated by a race of ");
 
         part26.Add("barely sentient ");
         part26.Add("(some would say) primitive ");
@@ -325,7 +329,7 @@ public class GameController : MonoBehaviour {
         part26.Add("feudal-capitalistic ");
         part26.Add("combative ");
 
-        part27.Add("game developers");
+        part27.Add("game developers ");
         part27.Add("boulders ");
         part27.Add("thistles ");
         part27.Add("energy fields ");
@@ -336,27 +340,31 @@ public class GameController : MonoBehaviour {
         part27.Add("rhubarb-compote ");
         part27.Add("odours ");
 
-        part28.Add("quite happy to trade us ");
+        part28.Add("who seem quite happy to trade us their ");
         part28.Add("who could be made to part with their ");
-        part28.Add("readily relieved of their ");
-        part28.Add("in rituals of ecstasy discarding heaps and heaps of ");
-        part28.Add("their elders showering us with ");
-        part28.Add("practically begging us to lift them to the stars in exchange for their ");
-        part28.Add("pelleting the probe with ");
-        part28.Add("honouring first contact by staging a display of ");
+        part28.Add("who will be readily relieved of their ");
+        part28.Add("who in rituals of ecstasy discard heaps and heaps of their ");
+        part28.Add("whose elders present to us their  ");
+        part28.Add("who are practically begging us to lift them to the stars in exchange for their ");
+        part28.Add("who vigorously pellet the probe with their ");
+        part28.Add("who have begun honouring first contact by staging a display of their ");
         part28.Add("who promise to grant our hearts' deepest desire, as long as that desire is for ");
-        part28.Add("swiftly swindled of their ");
+        part28.Add("who may be swiftly swindled of their ");
 
         part29.Add("banana peels.");
         part29.Add("bitcoins.");
         part29.Add("transcendental deductions.");
-        part29.Add("novel but shabby boardgames.");
+        part29.Add("shabby but kind of novel boardgames.");
         part29.Add("good grammar.");
         part29.Add("beer bottles.");
         part29.Add("upvotes.");
-        part29.Add("great composers' works.");
+        part29.Add("great nose-composers' works.");
         part29.Add("good vibrations.");
         part29.Add("most advanced shoes.");
+
+
+
+
 
 
 
@@ -367,11 +375,40 @@ public class GameController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+
+        // Cycle through dashboard lamps
+        lampCounter -= Time.deltaTime; 
+        if(lampCounter < 0)
+        {
+            IncrementLamp();
+            Debug.Log("Incrementing lamp");
+            lampCounter = 2.0f / 9;
+
+        }
+
 	    if(Input.GetKeyDown(KeyCode.G))
         {
             DiscoverNewPlanet();
         }
 	}
+
+    void IncrementLamp()
+    {
+        currentLamp += 1;
+        if(currentLamp >= lamps.Length)
+        {
+            currentLamp = 0;
+        }
+        lamps[currentLamp].GetComponent<SpriteRenderer>().sprite = lampSprites[1];
+
+        for (int i = 0; i < lamps.Length; i++)
+        {
+            if(i != currentLamp)
+            {
+                lamps[i].GetComponent<SpriteRenderer>().sprite = lampSprites[0];
+            }
+        }
+    }
 
     public void DiscoverNewPlanet()
     {
@@ -434,7 +471,7 @@ public class GameController : MonoBehaviour {
 
 
             worldText += part1[quintillions[0]];
-            worldText += part2[quintillions[1]];
+            worldText += part2[quintillions[1]]; // colour
             worldText += part3[quintillions[2]];
             worldText += part4[quintillions[3]];
             worldText += part5[quintillions[4]];
@@ -459,9 +496,9 @@ public class GameController : MonoBehaviour {
             worldText += part24[quintillions[23]];
             worldText += part25[quintillions[24]];
             worldText += part26[quintillions[25]];
-            worldText += part27[quintillions[24]];
-            worldText += part28[quintillions[25]];
-            worldText += part29[quintillions[25]];
+            worldText += part27[quintillions[26]];
+            worldText += part28[quintillions[27]];
+            worldText += part29[quintillions[28]];
 
      
 
@@ -472,15 +509,121 @@ public class GameController : MonoBehaviour {
             worldNumberText.GetComponent<Text>().text = "World: ???";
         }
         mainText.GetComponent<Text>().text = worldText;
-        ChangeCameraBackgroundColour();
+        ChangeCameraBackgroundColour(quintillions[1]);
     }
 
 
-void ChangeCameraBackgroundColour()
+void ChangeCameraBackgroundColour(int colourType)
     {
-        Camera.main.backgroundColor = new Color(Random.Range(0.2f, 0.5f), Random.Range(0.2f, 0.5f), Random.Range(0.2f, 0.5f));
+        float rMin = 0.2f, rMax = 0.4f, gMin = 0.2f, gMax = 0.4f, bMin = 0.2f, bMax = 0.4f;
+        
+        if(colourType == 0)
+        {
+            // pink
+            rMin = 0.99f;
+            rMax = 0.9f;
+            gMin = 0.2f;
+            gMax = 0.3f;
+            bMin = 0.65f;
+            bMax = 0.85f;
+        } else if (colourType == 1)
+        {
+            // green
+            rMin = 0.05f;
+            rMax = 0.15f;
+            gMin = 0.8f;
+            gMax = 0.99f;
+            bMin = 0.4f;
+            bMax = 0.55f;
+
+        }
+        else if (colourType == 2)
+        {
+            // grey
+            rMin = 0.34f;
+            rMax = 0.38f;
+            gMin = 0.34f;
+            gMax = 0.38f;
+            bMin = 0.34f;
+            bMax = 0.38f;
+        }
+        else if (colourType == 3)
+        {
+            // brown
+            rMin = 0.4f;
+            rMax = 0.6f;
+            gMin = 0.33f;
+            gMax = 0.42f;
+            bMin = 0.22f;
+            bMax = 0.34f;
+        }
+        else if (colourType == 4)
+        {
+            // blue
+            rMin = 0.2f;
+            rMax = 0.4f;
+            gMin = 0.25f;
+            gMax = 0.55f;
+            bMin = 0.7f;
+            bMax = 0.9f;
+        }
+        else if (colourType == 5)
+        {
+            // bland
+            rMin = 0.3f;
+            rMax = 0.4f;
+            gMin = 0.3f;
+            gMax = 0.4f;
+            bMin = 0.3f;
+            bMax = 0.4f;
+        }
+        else if (colourType == 6)
+        {
+            // orange 
+            rMin = 0.75f;
+            rMax = 0.95f;
+            gMin = 0.45f;
+            gMax = 0.65f;
+            bMin = 0.2f;
+            bMax = 0.3f;
+        }
+        else if (colourType == 7)
+        {
+            // yellow
+            rMin = 0.75f;
+            rMax = 0.9f;
+            gMin = 0.75f;
+            gMax = 0.9f;
+            bMin = 0.1f;
+            bMax = 0.3f;
+        }
+        else if (colourType == 8)
+        {
+            // red
+            rMin = 0.7f;
+            rMax = 0.9f;
+            gMin = 0.05f;
+            gMax = 0.25f;
+            bMin = 0.05f;
+            bMax = 0.25f;
+        }
+        else if (colourType == 9)
+        {
+            // purple
+            rMin = 0.55f;
+            rMax = 0.85f;
+            gMin = 0.3f;
+            gMax = 0.45f;
+            bMin = 0.6f;
+            bMax = 0.85f;
+        }
+
+
+        Camera.main.backgroundColor = new Color(Random.Range(rMin, rMax), Random.Range(gMin, gMax), Random.Range(bMin, bMax));
 
     }
+
+
 
     
 }
